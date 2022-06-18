fdbceb5cc9238c1f0a647d5f9f611ba1993d92a5
Tom Broad <tombroadyt@gmail.com>
2022-06-18 16:52:04 +0100

    added function into seperate file

---
+++ javascript/index.js
@@ -0,0 +1,40 @@
+// function that checks if a sport exists in the table and creates it if not
+function createMedalTable(medals) {
+  //create object to hold medal totals
+  const medalTable = {};
+  medals.map(sport => {
+    return sport.podium.map(country => {
+      //map returns automatically, forEach doesn't
+      country = country.slice(2)
+      if(!medalTable[country]) medalTable[country] = 0;
+    })
+  })
+  // helper function to add medals to table
+  addMedalsToTable(medals, medalTable);
+  // helper function to sort medal table into order
+  return sortTable(medalTable);
+}
+
+function addMedalsToTable(medals, medalTable) {
+  medals.map(sport => {
+    let counter = 0;
+    // The winner gets 3 points, second place 2 points and third place 1 point
+    sport.podium.forEach(country => {
+      country = country.slice(2);
+      counter === 0 ? medalTable[country] =  medalTable[country] + 3 
+        : counter === 1 ? medalTable[country] = medalTable[country] + 2
+          : counter === 2 ? medalTable[country] = medalTable[country] + 1
+            : console.log(`${country} is out of medal range in ${sport.sport}`);
+      counter++;
+    })
+  })
+}
+
+function sortTable(medalTable) {
+  const sortedTable = Object.fromEntries(
+    Object.entries(medalTable).sort(([,a],[,b]) => b - a)
+  );
+  return sortedTable;
+}
+
+module.exports = createMedalTable;
